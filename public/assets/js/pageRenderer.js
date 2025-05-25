e41988187d8fad9f88a8d95aa4b2562ff410f4ad
m41130 <45858598+m41130@users.noreply.github.com>
2025-05-25 14:04:52 +0200

    fix: preserve floating layout on public pages

--- public/assets/js/pageRenderer.js
+++ public/assets/js/pageRenderer.js
@@ -186,7 +186,7 @@ function ensureLayout(layout = {}, lane = 'public') {
 
       contentEl.innerHTML = '<div id="publicGrid" class="grid-stack"></div>';
       const gridEl = document.getElementById('publicGrid');
-      const grid = GridStack.init({ staticGrid: true }, gridEl);
+      const grid = GridStack.init({ staticGrid: true, float: true }, gridEl);
 
       matchedWidgets.forEach(def => {
         if (DEBUG) console.debug('[Renderer] render widget', def.id);
