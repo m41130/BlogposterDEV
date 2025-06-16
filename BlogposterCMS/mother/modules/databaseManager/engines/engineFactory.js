8e1923724c3464a4deca407fc173122bef7add81
m41130 <45858598+m41130@users.noreply.github.com>
2025-06-16 18:18:45 +0200

    fix sqlite startup crash

--- BlogposterCMS/mother/modules/databaseManager/engines/engineFactory.js
+++ BlogposterCMS/mother/modules/databaseManager/engines/engineFactory.js
@@ -2,15 +2,12 @@
  * mother/modules/databaseManager/engines/engineFactory.js
  */
 const { getDbType } = require('../helpers/dbTypeHelpers');
-const postgresEngine = require('./postgresEngine');
-const mongoEngine = require('./mongoEngine');
-const sqliteEngine = require('./sqliteEngine');
 
 function getEngine() {
   const type = getDbType();
-  if (type === 'postgres') return postgresEngine;
-  if (type === 'mongodb') return mongoEngine;
-  if (type === 'sqlite') return sqliteEngine;
+  if (type === 'postgres') return require('./postgresEngine');
+  if (type === 'mongodb') return require('./mongoEngine');
+  if (type === 'sqlite') return require('./sqliteEngine');
   throw new Error(`[engineFactory] Unknown DB type=${type}`);
 }
 
